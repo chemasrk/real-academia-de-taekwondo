b645be5e098a6a0140620534a28302cb7f83f76a
chemasrk <l3erserk@gmail.com>
2020-08-13 22:52:18 +0200

    mejoras diseño, ahora a cambiar imágenes y fin

--- src/pages/creator.tsx
+++ src/pages/creator.tsx
@@ -1,11 +1,29 @@
-import React from "react"
+import React, {useState} from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layouts/tkd"
+//import Layout from "../components/layouts/tkd"
 import SEO from "../components/seo"
 import "../components/layouts/new.css"
 
+const ChangeActive = () =>{
+
+  const classActive:string = "active"
+  const domMenu:Element = document.querySelector(".menuToggle")
+  const domNav:Element = document.querySelector(".navigation")
+
+  if(domMenu.classList.contains("active")){
+    domMenu.classList.remove("active")
+    domNav.classList.remove("active")
+  }else{
+    domMenu.classList.add("active")
+    domNav.classList.add("active")
+  }
+
+}
+
+
 const Creator = () => (
+
   <>
     <section>
       <div className="logo">Crative</div>
@@ -16,6 +34,24 @@ const Creator = () => (
         <a href="#">Get Started</a>
       </div>
       <p className="copyrightText">2020 chemi</p>
+      <div className="menuToggle" onClick={ChangeActive}></div>
+      <nav className="navigation">
+        <ul>
+          <li><Link to="/">Nosotros</Link></li>
+          <li><Link to="/">Clases</Link></li>
+          <li><Link to="/">Contacto</Link></li>
+          <li><Link to="/">Autor</Link></li>
+        </ul>
+        <div className="socialBar">
+          <ul>
+            <li><Link to="/"><img src="../facebook.png" /></Link></li>
+            <li><Link to="/"><img src="../instagram.png" /></Link></li>
+          </ul>
+          <a href="mailto:test@gmail.com" className="emailBx">
+            <img src="email.png" />
+          </a>
+        </div>
+      </nav>
     </section>
   </>
 )
