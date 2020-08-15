9d890066ae864ed07f0d47b1cbe263b2e9c43e5d
chemasrk <l3erserk@gmail.com>
2020-08-15 14:14:14 +0200

    mejoras

---
+++ src/components/layouts/intro.tsx
@@ -0,0 +1,112 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+import { Link } from "gatsby"
+import Img from "gatsby-image"
+
+import "./new.css"
+
+const ChangeActive = () =>{
+
+  const classActive:string = "active"
+  const domMenu:Element = document.querySelector(".menuToggle")
+  const domNav:Element = document.querySelector(".navigation")
+
+  if(domMenu.classList.contains("active")){
+    domMenu.classList.remove("active")
+    domNav.classList.remove("active")
+  }else{
+    domMenu.classList.add("active")
+    domNav.classList.add("active")
+  }
+
+}
+
+const Layout = ({ children }) => {
+  
+  const data = useStaticQuery(graphql`
+    query IntroLayoutQuery {
+      site {
+        siteMetadata {
+          title
+          description
+          author
+          domainurl
+          authorurl
+        }
+      }
+      allFile(filter: {extension: {regex: "/(jpg)|(jpeg)|(png)/"}, sourceInstanceName: {eq: "images"}}) {
+        edges {
+          node {
+            id
+            name
+            absolutePath
+            childImageSharp {
+              fluid(maxWidth: 1200) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  
+  const backgroundImg = data.allFile.edges.filter(img => img.node.absolutePath.includes("src/images/backgrounds/01.jpg"))[0].node.childImageSharp
+  const schoolLogo = data.allFile.edges.filter(img => img.node.name.includes("school_logo"))[0].node.childImageSharp
+  const instagramIcon = data.allFile.edges.filter(img => img.node.name.includes("instagram"))[0].node.childImageSharp
+  const emailIcon = data.allFile.edges.filter(img => img.node.name.includes("email"))[0].node.childImageSharp
+  const facebookIcon = data.allFile.edges.filter(img => img.node.name.includes("facebook"))[0].node.childImageSharp
+  
+  //const backgroundImg2 = data.allFile.edges.filter(img => img.node.absolutePath.includes("src/images/backgrounds/02.jpg"))[0].node.childImageSharp.fluid.src
+  
+
+  return (
+    <>
+      <section>
+        <div className="logo">{data.site.siteMetadata.title}</div>
+        <img 
+          className="bg"
+          src={backgroundImg.fluid.src}  />
+        <div className="content">
+          {children}
+        </div>
+        <p className="copyrightText">
+          <a target="_blank" href={data.site.siteMetadata.authorurl} title={data.site.siteMetadata.author}>{data.site.siteMetadata.author}</a>
+        </p>
+        <div className="menuToggle" onClick={ChangeActive}></div>
+        <nav className="navigation">
+          <ul>
+            <li><Link to="/">Nosotros</Link></li>
+            <li><Link to="/">Clases</Link></li>
+            <li><Link to="/">Contacto</Link></li>
+            <li><Link to="/">Autor</Link></li>
+          </ul>
+          <div className="socialBar">
+            <ul>
+              <li><Link to="/"><img src={facebookIcon.fluid.src} /></Link></li>
+              <li><Link to="/"><img src={instagramIcon.fluid.src} /></Link></li>
+            </ul>
+            <a href="mailto:test@gmail.com" className="emailBx">
+              <img src={emailIcon.fluid.src} />
+            </a>
+          </div>
+        </nav>
+      </section>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layouts/tkd"
-import Image from "../components/image"
+import Layout from "../components/layouts/intro.tsx"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <Layout test="123">
     <SEO title="Creator of this site" />
-    <a id="#test"></a>
-    <div className="one_half first">
-      <h6 className="heading">Ven a entrenar con nosotros</h6>
-      <p className="heading font-x3">Auctor diam sed non sapien in risus rhoncus egestas mauris rhoncus mi at odio in luctus turpis quis dignissim.</p>
-    </div>
-    <div className="one_half">
-      <ul className="nospace group">
-        <li className="one_half first btmspace-30">
-          <h6 className="heading">Interdum</h6>
-          <p>Placerat mi ipsum ornare dui id varius tortor est et urna vivamus commodo justo quis erat etiam lacus leo interdum non volutpat.</p>
-        </li>
-        <li className="one_half btmspace-30">
-          <h6 className="heading">Ullamcorper</h6>
-          <p>In sagittis vel erat donec congue pede eu lacus aliquam sodales felis nec libero mollis porttitor nulla adipiscing orci ac quam.</p>
-        </li>
-        <li className="one_half first">
-          <h6 className="heading">Accumsan</h6>
-          <p>Suspendisse elit praesent hendrerit lacus a condimentum scelerisque augue mi ultrices nunc vitae posuere orci dui quisque.</p>
-        </li>
-        <li className="one_half">
-          <h6 className="heading">Maecenas</h6>
-          <p>Tincidunt lectus vel enim aenean erat orci mollis quis gravida sed mollis a quam morbi libero sed ut erat aliquam erat volutpat.</p>
-        </li>
-      </ul>
-    </div>
+    <h2>Entrena con nosotros<br />El taekwondo tradicional en Palma de Mallorca</h2>
+    <p>Instructor Carlos Bazán VI Dan. Múltiple campeón mundial en blablablablablal... enseña en esta escuela el arte coreano por excelencia.</p>
+    <p>Ven a probar una clase sin compromiso</p>
+    <a href="#">Horarios</a>
   </Layout>
 )
 
